33ee6b10450eda011343335437c0a7fb7572331c
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-06 15:39:08 +0300

    logical operators

---
+++ logical opr.py
@@ -0,0 +1,52 @@
+#logical operators
+#not operator
+#1st program
+# x = True
+# y = not x
+# print(y)
+
+#2nd program
+# x = False 
+# y = not x
+# print(y)
+
+#3rd program
+# its_raining = False
+# if not its_raining:
+#     print("U can go out.")
+
+#and operators
+#4th program
+# x = True
+# y = False
+# z = x and y
+# print (z)
+
+#5th program 
+# age = 20
+# if age >= 18 and age <= 65:
+#     print("You are eligible to work.")
+
+#6th program
+# rain_chances = 80
+# if rain_chances > 50 and rain_chances < 100:
+#     print("There is a high chance of rain.")
+
+#7th program
+# speed = 70
+# if speed > 60 and speed < 80:
+#     print("You are driving at a safe speed.")
+
+#or operators
+#8th program
+# its_sunny = True
+# its_warm = False
+# if its_sunny or its_warm :
+#     print("Don't go outside.")
+
+#9th program
+its_cloudy = True
+its_rainy = False
+if its_cloudy or its_rainy:
+    print("Carry Umbrella with u.")
+
