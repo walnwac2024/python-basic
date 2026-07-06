c000303d3e9ae94732609ebd8343d144874d726c
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-06 08:41:45 +0300

    comparison operators done

---
+++ comparison ope.py
@@ -0,0 +1,41 @@
+#comparison operators 
+#1st program
+# num = 10
+# if num > 5:
+#     print("Num is greater than 5")
+
+#2nd program 
+# num1 = 10
+# num2 = 5
+# if num1 > num2:
+#     print("Num1 is greater than Num2")
+
+#3rd program
+# num1 = 10
+# num2 = 10
+# if num1 == num2:
+#     print("Num1 is equal to Num2")
+
+#4th program 
+# num1 =  10
+# num2 = 5
+# if num1 != num2:
+#     print("Num1 is not equal to Num2")
+
+#5th program
+# num1 = 10
+# num2 = 5
+# if num1 >= num2:
+#     print("Num1 is greater than or equal to Num2")
+
+#6th program
+# num1 = 5
+# num2 = 10
+# if num1 <= num2:
+#     print("Num1 is less than or equal to Num2")
+
+#7th program
+# num1 = 10
+# num2 = 5
+# if num1 > num2:
+#     print("Num1 is greater than Num2")
