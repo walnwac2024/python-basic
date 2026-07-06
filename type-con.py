6546c0404f5b7a95640cdb1930ad8244d155c410
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-06 06:57:58 +0300

    type con 10 examples

--- type-con.py
+++ type-con.py
@@ -1,4 +1,57 @@
-num_int = 5
-num_float = 3.14
-result = num_int + num_float
-print("the result of adding an integer and a float is:", result)
+#There are two types of type conversion in Python: implicit and explicit. Implicit type conversion is done automatically by Python, while explicit type conversion is done manually by the programmer using built-in functions.
+#1 explicit
+
+#1st program string to int
+# str_num = "10"
+# int_num = int(str_num)
+# print("the integer value of the string is:", int_num)
+
+#2nd program int to string
+# int_num = 42
+# str_num = str(int_num)
+# print("the string value of the integer is :",str_num)
+
+#3rd program int to float
+# int_num = 7
+# float_num = float(int_num)
+# print("the float value of the integer is :", float_num)
+
+#4th program float to int 
+# float_num = 3.99
+# int_num = int(float_num)
+# print("the integer value of the float is :", int_num)
+
+#5th program string to float
+# str_num = "3.14"
+# float_num = float(str_num)
+# print("the float value of the string is :", float_num)
+
+#6th program float to string
+# float_num = 2.718
+# str_num = str(float_num)
+# print("the string value of the float is :", str_num)
+# 
+# implicit
+# 7th program
+# num_int = 5 
+# num_float = 3.14
+# result = num_int + num_float
+# print("the result of adding an integer and a float is:", result)
+
+#8th program
+# num_int = 10
+# num_float = 2.5
+# result = num_int * num_float
+# print("the result of multiplying an integer and a float is:", result)
+
+#9th program
+# num_int = 7
+# num_float = 2.5
+# result = num_int / num_float
+# print("the result of dividing an integer by a float is:", result)
+
+#10th program
+num_int = 4
+num_float = 2.5
+result = num_int / num_float
+print("the result of dividing an integer by a float is:", result)
