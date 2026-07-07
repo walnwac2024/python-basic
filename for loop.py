6c95005cf371204c211a0a8cc290ab296980db66
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-07 15:10:39 +0300

    if elif

--- for loop.py
+++ for loop.py
@@ -1,11 +1,11 @@
 #1 program 
-# for i  in range(5):
-#   print("Hello World!")
+for i  in range(5):
+   print("Hello World")
 
 #2nd program
 # for i in range (10):
 #     print("it is raining")
 
 #3rd program
-for  i in range (40, 70):
-   print("good marks")
+# for  i in range (40, 70):
+#    print("good marks")
