d99a18466b1a6073ef50a2da0f2c0df3fc44a017
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-09 17:53:07 +0300

    While Loop

---
+++ while loop.py
@@ -0,0 +1,23 @@
+#1 program 
+# reply = ""
+# while reply != "Yes":
+#     reply = ("are we there yet?")
+#     print("yayy! we arrived")
+
+#2 program 
+# age =""
+# while age != "18":
+#     age =input("Can I Get a license?Enter Age:")
+#     print ("yes u r eligible.")
+ 
+#3rd program
+# chances = ""
+# while chances !=">70%":
+#     chances =input("Is there is any chances of rain:")
+#     print("Yes.")
+
+#4th program
+count =1
+while count <=5:
+    print(f"Iteration number:{count} ")
+    count +=1
