169706c5d689ac15693424e94460c741d22e8ebc
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-09 20:54:46 +0300

    lists in python

---
+++ lists.py
@@ -0,0 +1,32 @@
+#lists of changeable
+#1 program
+# fruits = ["banana" , "cherry", "apple", "strawberry"]
+# numbers = [21 , 22 ,23 ,24]
+# mixed_list = fruits + numbers
+# print (mixed_list)
+
+#2nd program
+#fruits = ["banana" , "cherry", "apple", "strawberry"]
+# print(fruits[1])
+
+
+#3rd program
+# fruits = ["banana" , "cherry", "apple", "strawberry"]
+# fruits[1] ="blueberry"
+# print(fruits)
+
+#4th program
+# fruits = ["banana" , "cherry", "apple", "strawberry"]
+# fruits.append ("orange")
+# print(fruits)
+
+#5th program
+# fruits = ["banana" , "cherry", "apple", "strawberry"]
+# fruits.insert (1,"orange")
+# print(fruits)
+
+#6th program
+# fruits = ["banana" , "cherry", "apple", "strawberry"]
+# fruits.remove("orange")
+# print(fruits)
+
