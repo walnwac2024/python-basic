ff85d10f60d32fa32d8109b08210c72fc8b87367
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-09 21:15:07 +0300

    Tuples in Python

---
+++ tuple.py
@@ -0,0 +1,13 @@
+#1
+# x=10
+# y=30
+# x , y= y , x
+# print( f"x:{x}, y:{y}")
+#2
+# students = [("ali" , 87), ("waleed" , 92), ("umair" , 88)]
+# for name, marks in students :
+#     print(f"{marks} marks {name}")
+#3
+# fruit = [("mango",200),("apple",250),("tomato",400)]
+# for name, price in fruit:
+#     print(f"{price} price{name}")
