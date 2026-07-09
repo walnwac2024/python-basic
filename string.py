fb990bfd4474325d41a1b2afad53ae7e3d0ee917
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-09 18:15:21 +0300

    string in python

---
+++ string.py
@@ -0,0 +1,31 @@
+#single quotes
+#program 1
+# name ='Ali'
+# print(name)
+
+#program2
+# Rollno = 'BS AI UOH'
+# print(Rollno)
+
+#double quotes
+#3rd program
+# greet = "Hey! How r U?"
+# print(greet)
+
+#4th program
+# msg = "There are chances of rain.so please carry an umbreela with u."
+# print(msg)
+
+#triple qoutes
+#5th program
+# msg = """ this is an example of 
+# multi line string"""
+# print(msg)
+
+#string concatenation
+first_name = "Ali"
+last_name = "Ozaifa"
+full_name = first_name + " " + last_name
+print (full_name)
+
+
