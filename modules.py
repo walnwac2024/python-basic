e800211a5f749a737863d84b78a514ce0683fc60
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-10 08:41:51 +0300

    Modules in Python

---
+++ modules.py
@@ -0,0 +1,36 @@
+# import math
+# print(math.factorial(6))
+
+#2 program
+# import math
+# print(math.ceil(9.007))
+
+#3 program
+# import random
+# cars =["BMW" , "TOYOTA" , "JAGUAR" , "CIVIC"]
+# print(random.choice(cars))
+
+#4 program
+# import random
+# cars =["BMW" , "TOYOTA" , "JAGUAR" , "CIVIC"]
+# print(random.randint(1, 6))
+
+#5 program
+# import statistics as stats
+# price = [45, 67, 95 , 59, 71]
+# print(stats.mean(price))
+
+#6 program
+# import statistics as stats
+# price = [45, 67, 95 , 59, 71]
+# print(stats.median(price))
+
+#7 program
+# from datetime import datetime
+# now = datetime.now()
+# print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+#8 program
+# from datetime import datetime
+# old_time = datetime(2005, 11, 10, 12, 50,31)
+# print(old_time.strftime("%Y-%m-%d %H:%M:%S"))
