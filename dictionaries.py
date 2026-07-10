93d683890d21b1eed6beda28fd638da06334faaf
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-10 07:23:45 +0300

    Dictionaries in python

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+#1 program
+# my_data ={
+#     "name" : "Ali",
+#     "Roll No" : "BsAi-140",
+#     "email" : "alihuzaifa1276@gmail.com"
+# }
+# print(my_data["email"])
+
+#2 program
+# shopping = {"apple":200 , "mango":300 , "cherry":500}
+# shopping["apple"]=250
+# print(shopping)
+
+#3rd program
+# shopping = {"apple":200 , "mango":300 , "cherry":500}
+# del shopping["apple"]
+# print(shopping)
+
+#4th program
+# prices = {"apple":200 , "mango":300 , "cherry":500}
+# print (prices.get("apple"))
+# print (prices.get("strawberry"))
+# print (prices.get("strawberry" , 400))
+
