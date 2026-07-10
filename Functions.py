fa94e2c73c4719bcb781e64a9a8284c3a71db99a
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-10 07:37:55 +0300

    Functions in python

---
+++ Functions.py
@@ -0,0 +1,23 @@
+#1 program
+# def my_data():
+#     print("Hello! my name is ali and i am an Ai engineer")
+# my_data()
+
+# 2 program
+# def greet_person(name):
+#     print(f"Hello, {name}!")
+# greet_person("Ali")
+# greet_person("Waleed")    
+
+#3rd program
+# def greet_with_city(name , city="Jand"):
+#     print(f"Hello, {name} is from {city}.")
+# greet_with_city("Ali","Wah")
+# greet_with_city("Waleed")  
+
+#4th Program
+# def multiply_num(a,b):
+#     return  a*b
+# result = multiply_num(5,7)
+# print(result)
+
