c45cf224bc335ec570dcab45c763f8eef39e3a7f
ALI OZAIFA <alihuzaifa1276@gmail.com>
2026-07-10 08:05:50 +0300

    exception Handling in python

---
+++ exception.handling.py
@@ -0,0 +1,25 @@
+#1 program
+# try:
+#     number = int(input("Enter a number:"))
+#     result =10/number
+#     print(f"result is {result}")
+# except:
+#     print("OOPS,Something went wrong.")
+
+#2 program
+# try:
+#     number = int(input("Enter a number:"))
+#     result =10/number
+#      print(f"result is {result}")
+# except ZeroDivisionError:
+#     print("OOPS,Something went wrong.")
+
+#3 program
+# try:
+#     number = int(input("Enter a number:"))
+#     result =10/number
+#     print(f"result is {result}")
+# except ValueError:
+#    print("OOPS,Something went wrong.")
+
+
